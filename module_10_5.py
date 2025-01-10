31f6aafd42fc98964195811a6f5ca6bb331bdacb
KKategor <kamchatkasweethome@gmail.com>
2025-01-10 11:10:35 +0300

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -1,21 +1,26 @@
+# Task "Многопроцессное программирование"
+
 from datetime import datetime
 from multiprocessing import Pool
 
-def read_info(name):
-    all_data = []
-    with open(name) as file:
+def read_info(filename):
+    data = []
+    with open (filename, 'r', encoding= 'utf-8') as file:
         for line in file:
-            all_data.append(line)
+            data.append(line)
+    return data
 
 if __name__ == '__main__':
     filenames = [f'./file {number}.txt' for number in range(1, 5)]
 
     start_time = datetime.now()
-    for name in filenames:
-        read_info(name)
-    print(datetime.now() - start_time, '(линейный)')
+    linear_data = []
+    for filename in filenames:
+        linear_data.extend(read_info(filename))
+    print(datetime.now() - start_time, 'линейный метод')
 
     start_time = datetime.now()
     with Pool(processes=4) as pool:
-        pool.map(read_info, filenames)
-    print(datetime.now() - start_time, '(многопроцессный)')
+        all_data = pool.map(read_info, filenames)
+    combined_data = [line for data in all_data for line in data]
+    print(datetime.now() - start_time, 'многопроцессный метод')
